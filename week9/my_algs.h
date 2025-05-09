3b2090161604904887a60e251d7fed4c9885e751
20211992 <psoz0402@naver.com>
2025-05-09 17:05:38 +0900

    fin

---
+++ week9/my_algs.h
@@ -0,0 +1,31 @@
+#pragma once
+//my_algs.h
+#ifndef GUARD_my_algs_h
+#define GUARD_my_algs_h
+
+
+
+// 최댓값 함수
+template<class T>
+T max(const T& left, const T& right) {
+	return left > right ? // 조건?
+		left : right;		//참 반환 : 거짓 반환
+}
+
+// 최솟값 함수 (min)
+template<class T>
+T min(const T& left, const T& right) {
+	return left < right ? // 조건?
+		left : right;		//참 반환 : 거짓 반환
+}
+
+
+// 같으면 함수 (equals)
+template<class T>
+T equals(const T& left, const T& right) {
+	return left == right ? // 조건?
+		true : false;		//참 반환 : 거짓 반환
+}
+
+
+#endif
